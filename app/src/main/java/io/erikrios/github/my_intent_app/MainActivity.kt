2bd85047b9f21dc95b3a9ffa885fd5b2f755b0bc
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-19 19:40:22 +0700

    Integrated the View Binding in MainActivity.kt

--- app/src/main/java/io/erikrios/github/my_intent_app/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/my_intent_app/MainActivity.kt
@@ -1,11 +1,17 @@
 package io.erikrios.github.my_intent_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.erikrios.github.my_intent_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
